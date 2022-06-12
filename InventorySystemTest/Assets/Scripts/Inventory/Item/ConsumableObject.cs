691e98266b8be27408cef4dc95dee06ce0c42b80
Niklas Jörgensen <niklio15csgo@hotmail.com>
2022-06-12 16:50:38 +0200

    reworked items a bit and started work on randomized items

--- InventorySystemTest/Assets/Scripts/Inventory/Item/ConsumableObject.cs
+++ InventorySystemTest/Assets/Scripts/Inventory/Item/ConsumableObject.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Consumable Object", menuName = "Inventory System/Items/Consumable")]
 public class ConsumableObject : ItemData
 {
-    public int restoreHealthValue, restoreManaValue, increaseStrength;
-
     public void Awake()
     {
         type = ItemType.Consumable;
