73f67476d6503afee8a7931d42a577289e20ce88
EricLocher <eriloc1129@skola.goteborg.se>
2022-06-24 15:46:30 +0200

    Dungeon Generation Draft

--- InventorySystemTest/Assets/Scripts/Eric/Tilesets/Scripts/Room.cs
+++ InventorySystemTest/Assets/Scripts/Eric/Tilesets/Scripts/Room.cs
@@ -1,14 +1,40 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Room : MonoBehaviour
 {
-    [HideInInspector] public List<Doorway> doorsways;
+    public Vector2 center;
+    public int width, height;
 
-    private void Start()
+    [SerializeField] public List<Doorway> doorsways;
+
+    float startRot = 0;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    public void Init()
     {
         doorsways = new List<Doorway>(transform.GetComponentsInChildren<Doorway>());
+        startRot = transform.eulerAngles.y;
+    }
+
+    public Doorway RandomDoor()
+    {
+        for (int i = 0; i < doorsways.Count; i++) {
+            if(doorsways[i].isConnected) { doorsways.RemoveAt(i); i--; }
+        }
+
+        return doorsways[Random.Range(0, doorsways.Count)];
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(new Vector3(center.x, 0, center.y), new Vector3(width, 0, height));
     }
 
 }
--- InventorySystemTest/Assets/Scripts/Eric/Tilesets/Scripts/Tileset.cs
+++ InventorySystemTest/Assets/Scripts/Eric/Tilesets/Scripts/Tileset.cs
@@ -1,11 +1,28 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Tileset", menuName = "Dungeon/Tileset")]
 public class Tileset : ScriptableObject
 {
-    public string name;
+    public new string name;
+    [Header("Start Room")]
+    public Room StartRoom;
+    [Header("End Room")]
+    public Room EndRoom;
 
+    [Space]
     public List<Room> tiles;
+
+    public void Init()
+    {
+        foreach (Room room in tiles) {
+            room.Init();
+        }
+    }
+
+    public Room RandomRoom()
+    {
+        return tiles[Random.Range(0, tiles.Count)];
+    }
+
 }
